fc31e3a6bb915b7b8531c8c753ef817b34f8c92f
YZT1997 <yangzt_0943@163.com>
2021-05-23 09:27:35 +0800

    lingang experiment

---
+++ learning_communication/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/sunh/yammar_ws/src/learning_communication
+# Build directory: /home/sunh/yammar_ws/src/learning_communication/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ learning_communication/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/sunh/yammar_ws/src/learning_communication/build/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/melodic/bin:”/home/sunhan/clion-2020.3.1/bin/:/home/sunh/anaconda3/bin:/home/sunh/.local/bin:/home/sunh/anconda3/bin:/usr/local/cuda/bin:/home/sunh/anaconda3/condabin:/usr/local/bin:/usr/bin:/bin:/usr/games:/snap/bin:/usr/local/cuda/bin”'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/sunh/yammar_ws/src/learning_communication/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/sunh/yammar_ws/src/learning_communication:$ROS_PACKAGE_PATH"
